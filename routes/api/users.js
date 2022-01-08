783fa4c686b687c331812fc18a3eb21a316463c4
Shivam <shivam.gup82@gmail.com>
2022-01-08 17:08:32 +0530

    added JWT token

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -52,15 +54,25 @@ router.post(
 
       await user.save();
 
-      res.send('User registered');
-      // Encrypt password
-      // Return JWT
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
       res.status(500).send('Server error');
     }
-
-    res.send('User Route');
   }
 );
 
