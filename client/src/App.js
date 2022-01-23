c8b7c8a0eafdc9c565e3767112393efb818e6bc0
Shivam <shivam.gup82@gmail.com>
2022-01-23 06:28:49 +0530

    Set Auth Token

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,22 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
