8bb7bb39edc1186dbb582b42b8886bff1b1a0fb8
Shivam <shivam.gup82@gmail.com>
2022-01-24 21:02:15 +0530

    Get posts action and reducer

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -11,6 +11,9 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         <Link to='/profiles'>Developers</Link>
       </li>
       <li>
+        <Link to='/posts'>Posts</Link>
+      </li>
+      <li>
         <Link to='/dashboard'>
           <i className='fas fa-user'></i>{' '}
           <span className='hide-sm'>Dashboard</span>
@@ -27,7 +30,6 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 
   const guestLinks = (
     <ul>
-      <Link to='/profiles'>Developers</Link>
       <li>
         <Link to='#!'>Developers</Link>
       </li>
